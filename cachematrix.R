89eafb3a9b2d370d8fcf688506ad02d6425d626f
rajaramshinde <rajaramshinde@hotmail.com>
2016-07-02 22:10:39 +0100

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -3,8 +3,9 @@
 #function do:
 #1.set the value of the matrix
 #2.get the value of the matrix
-#3.set the value of the mean
-#4.get the value of the mean
+#3.set the value of the matrix
+#4.get the value of the matrix
+
 makeCacheMatrix <- function(x = matrix()) {
   
   m<-NULL
